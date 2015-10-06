fc9cccd4cde4d94608a3d7c80ba017cbac7dae05
Joseph Wang <jamesxxxxxxx@gmail.com>
2015-10-06 12:22:54 +0000

    change get to post

--- index.js
+++ index.js
@@ -274,12 +274,12 @@ app.get('/api/queryPost', function(request, response) {
   //註冊事件監聽器 搜尋結果轉換成陣列後監聽器會被觸發
   items.find().forEach(
       function(newPost){
-        newPost.userID=database.member.findOne({"userName":newPost.userName});
+        newPost.userID=database.member.findOne({"userID":newPost.userID});
         database.PostReloaded.insert(newPost);
       }
     );
   
-  database.PostReloaded.find().pretty().sort({$natural: -1}).limit(limit).toArray(function (err, docs) {
+  database.PostReloaded.find().sort({$natural: -1}).limit(limit).toArray(function (err, docs) {
     //若事件觸發器收到有錯誤，就使用 __sendErrorResponse()回傳錯誤
     if (err) {
       console.log(err);
